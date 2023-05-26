5110942fe20584d7917e4b19ad127bc4b5664f38
jhagg26 <jhagg26@gmail.com>
2023-05-25 20:15:45 -0400

    Add JS custom validation for matching passwords

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-const form = document.querySelector("form");
+const passwordDOM = document.getElementById("password");
 let password = "";
 let confirmPassword = "";
 
-// Reference: https://javascript.plainenglish.io/how-to-get-html-form-values-with-javascript-b4869bc5e889
-form.addEventListener("submit", (event) => {
+// References: https://javascript.plainenglish.io/how-to-get-html-form-values-with-javascript-b4869bc5e889
+// https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation
+passwordDOM.addEventListener("input", (event) => {
+  
+    password = document.querySelector("input#password").value;
+    console.log(password);
 
-    event.preventDefault()
-    const formData = new FormData(form)
-    for (const pair of formData.entries()) {
-    
-        if(pair[0] == 'password') {
-            password = pair[1];
-        }
+    confirmPassword = document.querySelector("input#confirm_password").value;
+    console.log(confirmPassword);
 
-        if(pair[0] == 'confirm-password') {
-            confirmPassword = pair[1];
-        }
-  }
-    
-
-  if(password.localeCompare(confirmPassword) == 0) {
+    if(password.localeCompare(confirmPassword) == 0) {
     console.log("Passwords match");
-  } 
-  else {
+    passwordDOM.setCustomValidity("");
+    } 
+    else {
     console.log("Passwords do NOT match");
-  }
+    passwordDOM.setCustomValidity("Passwords do not match!");
+    }   
 });
